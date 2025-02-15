c0e66fd60482f866af377f0216fe25243631d923
arghaghosh12 <ar.arghaghosh@gmail.com>
2025-02-15 19:40:29 +0530

    adding  user entity

---
+++ src/main/java/com/example/gitdemo/model/User.java
@@ -0,0 +1,22 @@
+package com.example.gitdemo.model;
+
+public class User {
+    private int userId;
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
